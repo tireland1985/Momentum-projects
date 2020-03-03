93ff974acb83014c6be03d90f5f7e80dcbac2b8d
tireland1985 <tim.ireland19@my.northampton.ac.uk>
2020-03-03 10:51:33 +0000

    03/03/2020@10:51

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+
+/* ToDoList
+*/
+window.onload = function() {
+        //variables
+        let form = document.getElementById("form");
+        let form = document.getElementById("input");
+        let form = document.getElementById("btn");
+        let form = document.getElementById("list");
+        let form = document.getElementById("btnClear");
+        let id = 1;
+
+        let liItem = "";
+        let toDoList = [];
+
+        btn.addEventListener("click", addTodoItem);
+        list.addEventListener("click", boxChecked);
+        btnClear.addEventListener("click", 
+
+}
